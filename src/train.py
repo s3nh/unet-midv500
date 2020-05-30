23e947ad3a1080b6a42cbb2c857e4932fe3c6250
s3nh <steam.panek@gmail.com>
2020-05-30 22:23:59 +0200

    Add resnet34 unet

--- src/train.py
+++ src/train.py
@@ -11,14 +11,6 @@ import torch.nn.functional as F
 def train_model(model, optimizer, scheduler, num_epochs):
     for epoch in range(num_epochs):
         since = time.time()
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                for param_group in optimizer.param_groups:
-                    print("LR", param_group['lr'])
-                model.train()
-            else:
-                model.eval()
 
         metrics = defaultdict(float)
         epoch_samples = 0
@@ -28,17 +20,11 @@ def train_model(model, optimizer, scheduler, num_epochs):
             labels = labels.to(device)
             optimizer.zero_grad()
 
-            with torch.set_grad_enabled(phase == 'train'):
-                outputs = model(inputs)
-                loss = calc_loss(ouput, labels, metrics)
-                loss.backward()
-                optimizer.step()
-            epoch_samples += inputs.size(0)
+            outputs = model(inputs)
+            loss = calc_loss(ouput, labels, metrics)
+            loss.backward()
+            optimizer.step()
 
-        epoch_loss = metrics['loss'] / epoch_samples
-        if phase == 'val' and epoch_loss < best_loss:
-            best_loss = epoch_loss 
-            best_model_wts = copy.deepcopy(model.state_dict())
 
-    model.load_state_dict(best_model_wts)
+    #model.load_state_dict(best_model_wts)
     return model 
