85152c58582b3c61c160e0fc91125a77336eefd8
s3nh <steam.panek@gmail.com>
2020-05-31 16:05:57 +0200

    Add swish activation

--- models/unet.py
+++ models/unet.py
@@ -4,23 +4,33 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.models 
 
-def swish(x):
-    return x * F.sigmoid(x)
-
 # Double convolutional block 
 # bypassed with Swish activation function, 
 # insted of Relu as state in paper 
 # TODO: Think about how to change params to store it into a dictionary 
+# Define Silu activation function 
+
+
+def swish(input):
+
+    return input * torch.sigmoid(input)
+
+class Swish(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return swish(input)
+
 
 def dconv(in_channels, out_channels):
     return nn.Sequential(
-
         nn.Conv2d(in_channels, out_channels, kernel_size = 3, padding = 1), 
-        swish(), 
+        Swish(), 
         nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1), 
-        swish()
+        Swish(), 
     )
-
 
 class UNet(nn.Module):
     def __init__(self, n_class):
